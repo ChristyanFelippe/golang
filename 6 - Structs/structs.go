e28cfb8327d7799856104f5aef7c8ee5176da03f
ch052877 <christyanmend@gmail.com>
2023-08-06 01:05:27 -0300

    structs lesson

--- 6 - Structs/structs.go
+++ 6 - Structs/structs.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type usuario struct {
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
+type endereco struct {
+	logradouro string
+	numero     uint8
+}
+
 func main() {
-	fmt.Println("oi")
+	var u usuario
+	fmt.Println(u)
+	u.nome = "Christyan"
+	u.idade = 30
+	fmt.Println(u)
+
+	EnderecoExemplo := endereco{"Rua lobos", 0}
+
+	usuario2 := usuario{"Christyan", 30, EnderecoExemplo}
+	fmt.Println(usuario2)
+
+	usuario3 := usuario{nome: "Christyan"}
+	fmt.Println(usuario3)
 }
---
+++ 7 - Herança so que nao/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome  string
+	idade uint8
+}
+
+type estudante struct {
+	id uint8
+}
+
+func main() {
+	fmt.Println("Herança")
+}
