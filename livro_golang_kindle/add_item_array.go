2ecef65bcfae8f9953081c7f3a39a5d3e644760d
ch052877 <christyanmend@gmail.com>
2024-03-08 00:59:07 -0300

    exemplo 1 do livro de golang

---
+++ livro_golang_kindle/add_item_array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var valor_a_ser_adicionaro uint = 30
+	meuArray := [5]int{10, 20, 30, 40, 50}
+	fmt.Println(meuArray, len(meuArray))
+	var novoArray [len(meuArray) + 1]int
+	fmt.Println(len(novoArray))
+	for i := 0; i < len(meuArray); i++ {
+		novoArray[i] = meuArray[i]
+	}
+	fmt.Println("array atualizado ", novoArray)
+	novoArray[len(novoArray)-1] = int(valor_a_ser_adicionaro)
+	fmt.Println("final ", novoArray)
+
+}
