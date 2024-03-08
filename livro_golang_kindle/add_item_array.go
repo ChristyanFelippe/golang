59790f2124ce3e20efc8ced2f877d38e211d1476
ch052877 <christyanmend@gmail.com>
2024-03-08 01:14:54 -0300

    removendo prints

--- livro_golang_kindle/add_item_array.go
+++ livro_golang_kindle/add_item_array.go
@@ -5,9 +5,7 @@ import "fmt"
 func main() {
 	var valor_a_ser_adicionaro uint = 30
 	meuArray := [5]int{10, 20, 30, 40, 50}
-	fmt.Println(meuArray, len(meuArray))
 	var novoArray [len(meuArray) + 1]int
-	fmt.Println(len(novoArray))
 	for i := 0; i < len(meuArray); i++ {
 		// novoArray[i] = meuArray[i]
 		copy(novoArray[:], meuArray[:])
